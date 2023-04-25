9254f4e848c69bfe9f661f2fdf2031f0e28084ce
Stan Potemkin <bronenos@gmail.com>
2023-04-25 10:29:37 +0300

    Fix the JivoFoundation building #2

--- Shared/Models/JsonElement+Proto.swift
+++ Shared/Models/JsonElement+Proto.swift
@@ -14,36 +14,6 @@ public extension JsonElement {
         return self
     }
     
-    func has(key: String) -> JsonElement? {
-        let value = self[key]
-        return value.exists(withValue: false) ? value : nil
-    }
-
-    var stringToStringMap: [String: String]? {
-        return ordict?.unOrderedMap.compactMapValues { $0.string }
-    }
-
-    var stringArray: [String] {
-        return arrayValue.compactMap { $0.string }
-    }
-
-    var intArray: [Int]? {
-        return array?.compactMap { $0.number?.intValue }
-    }
-
-    var valuable: String? {
-        return stringValue.jv_valuable
-    }
-    
-    func map<T>(_ block: (JsonElement) -> T) -> T? {
-        if exists(withValue: true) {
-            return block(self)
-        }
-        else {
-            return nil
-        }
-    }
-    
     func parse<T: JVDatabaseModelChange>(force: Bool = false) -> T? {
         if exists(withValue: true) {
             let change = T(json: self)
@@ -57,8 +27,4 @@ public extension JsonElement {
     func parseList<T: JVDatabaseModelChange>() -> [T]? {
         return array?.map { T(json: $0) }
     }
-}
-
-public func +(lhs: JsonElement, rhs: JsonElement) -> JsonElement {
-    return lhs.merged(with: rhs)
 }
--- Shared/Sources/Extensions/System/JsonElement+.swift
+++ Shared/Sources/Extensions/System/JsonElement+.swift
@@ -16,4 +16,38 @@ public extension JsonElement {
             self.init(Dictionary<String, T>())
         }
     }
+    
+    func has(key: String) -> JsonElement? {
+        let value = self[key]
+        return value.exists(withValue: false) ? value : nil
+    }
+
+    var stringToStringMap: [String: String]? {
+        return ordict?.unOrderedMap.compactMapValues { $0.string }
+    }
+
+    var stringArray: [String] {
+        return arrayValue.compactMap { $0.string }
+    }
+
+    var intArray: [Int]? {
+        return array?.compactMap { $0.number?.intValue }
+    }
+
+    var valuable: String? {
+        return stringValue.jv_valuable
+    }
+    
+    func map<T>(_ block: (JsonElement) -> T) -> T? {
+        if exists(withValue: true) {
+            return block(self)
+        }
+        else {
+            return nil
+        }
+    }
+}
+
+public func +(lhs: JsonElement, rhs: JsonElement) -> JsonElement {
+    return lhs.merged(with: rhs)
 }
